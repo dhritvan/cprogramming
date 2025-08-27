bf92effa43a4ebf8051b5951e65906ae5bd44a15
dhritvan <dhriturajde1234@gmail.com>
2025-08-27 23:44:10 +0530

    Create fahrenhittocelsius.c

---
+++ lab/fahrenhittocelsius.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float f;
+    printf("Enter Fahrenheit: ");
+    scanf("%f", &f);
+    printf("Celsius = %.2f\n", (5.0f/9.0f) * (f - 32.0f));
+    return 0;
+}
